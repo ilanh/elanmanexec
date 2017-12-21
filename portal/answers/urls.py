362bc769d3d421e99f3da240011b35c55738df17
Ilan Harel <ilanharel@gmail.com>
2017-12-21 19:55:55 +0200

    begin work on site

---
+++ portal/answers/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from .views import *
+
+app_name = 'answers'
+
+urlpatterns = [
+    url(r'^$', PublicContribListView.as_view(), name='index'),
+    url(r'^create/$', PublicContribCreateView.as_view(), name='create'),
+    url(r'^(?P<slug>[\w-]+)/$', PublicContribDetailView.as_view(), name='get'),
+    url(r'^(?P<slug>[\w-]+)/edit/$', PublicContribUpdateView.as_view(), name='edit'),
+    url(r'^(?P<slug>[\w-]+)/del/$', PublicContribDeletelView.as_view(), name='del'),
+]
--- portal/portal/settings.py
+++ portal/portal/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'portal.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -74,24 +74,24 @@ WSGI_APPLICATION = 'portal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'devdb',  # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': 'devuser',
-        'PASSWORD': 'devpass',  # Entered via fab command; leave blank if using SQLite
-        'HOST': '',  # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',  # Set to empty string for default.
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+#         'NAME': 'devdb',  # Or path to database file if using sqlite3.
+#         # The following settings are not used with sqlite3:
+#         'USER': 'devuser',
+#         'PASSWORD': 'devpass',  # Entered via fab command; leave blank if using SQLite
+#         'HOST': '',  # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+#         'PORT': '',  # Set to empty string for default.
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
@@ -139,5 +139,5 @@ STATIC_ROOT = '/static/';
 
 # Import production settings if the environment variable DJANGO_PRODUCTION is true
 # (It's set to True by default in the Dockerfile, but you can override it with `docker run` for development)
-if os.environ['DJANGO_PRODUCTION'] == 'true':
-    from settings_production import *
+# if os.environ['DJANGO_PRODUCTION'] == 'true':
+#     from settings_production import *
