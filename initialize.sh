ff8c10fd70db84e4dd65e934cdffc0bd477616bd
Ilan Harel <ilanharel@gmail.com>
2017-12-23 19:10:17 +0200

    Update initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+# Initialize Django project
 python3 /code/portal/manage.py collectstatic --noinput
 python3 /code/portal/manage.py makemigrations
 python3 /code/portal/manage.py migrate --noinput
